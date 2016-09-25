7c92d0a9cfcfc6d87842fa81f022b7c24f2065b8
gabrielArakaki <gabriel@revmob.com>
2016-09-25 18:49:12 -0300

    last version with word groupping

--- script.js
+++ script.js
@@ -1,77 +1,179 @@
 var fs = require('fs')
 var _ = require('lodash')
-var async = require('async')
+var json2csv = require('json2csv')
 
-var files = [{name: '2014.csv', index: 43}, {name: '2015.csv', index: 39}, {name: '2016.csv', index: 39}]
-
-var wordMap = {}
-var collection = []
+var file = {name: '2014.csv', index: 41}
 
 var exceptionWords = ['', 'de', 'mais', 'do', 'e', 'para', 'o', 'a', 'com', 'um', 'que', 'em', 'os', 'da', 'ser', 'ter',
-'no', 'uma', 'na', 'dos', 'do', 'por', 'está', 'as', 'é', 'ou']
+'no', 'uma', 'na', 'dos', 'do', 'por', 'está', 'as', 'é', 'ou', 'das', 'ao', 'sobre', 'me', 'foi']
 
 var casualException = ['curso', 'aula', 'cursos', 'aulas']
 var lineCount = 0
+var totalLineCount = 0
 
-function groupWordMelhorias (word) {
-  if (['tempo', 'duração', 'horas'].indexOf(word) !== -1 ) return 'tempo'
-  if (['disponibilizar', 'disponibilização', 'disponibilizado', 'disponibilidade'].indexOf(word) !== -1 ) return 'disponibilizar'
-  if (['prática', 'práticas', 'práticos', 'prático', 'pratica', 'praticas', 'praticos', 'pratico'].indexOf(word) !== -1 ) return 'pratica'
-  if (['horário', 'horária', 'horários', 'horario', 'horaria', 'horarios'].indexOf(word) !== -1 ) return 'horario'
-  if (['dinâmica', 'dinâmicas', 'dinamica', 'dinamicas'].indexOf(word) !== -1 ) return 'dinamica'
+var wordMap = {}
+var collection = []
+var sinergyWordMap = {}
+var sinergyWordMapFiltered = {}
+var finalJsonToCsv = []
 
-  return word
+var MAX_WORDS_SINERGY = 3
+var MIN_WORD_COUNT = 0.01
+
+var GROUP_WORDS_DICTIONARY = {
+  conhecimento : ['conhecimento', 'conhecimentos'],
+  tecnologia : ['tecnologia', 'tecnologias'],
+  novo : ['nova', 'novas', 'novos', 'novo'],
+  ferramenta : ['ferramenta', 'ferramentas'],
+  app : ['app', 'apps', 'aplicativo', 'aplicativos'],
+  professor : ['professor', 'instrutor']
 }
 
-async.eachSeries(files, function(file, callback) {
-
-  fs.readFile('./' + file.name, 'utf-8', function(err, data) {
-    data = data.split('\n')
-    data.forEach(function(line, index) {
-      if (index === 0) return
-
-      var response = line.split(',')[file.index]
-      var wordArray = response.split(' ')
-      if (wordArray.length > 1 || wordArray[0] !== '') {
-        wordArray.forEach(function(word) {
-          var formattedWord = word.toLowerCase()
-          if (formattedWord[formattedWord.length - 1] === '.') formattedWord = formattedWord.replace(/\./, '')
-
-          if (exceptionWords.indexOf(formattedWord) !== -1 || casualException.indexOf(formattedWord) !== -1) return
-
-          //formattedWord = groupWord(formattedWord)
-
-          if(!wordMap[formattedWord]) wordMap[formattedWord] = {count : 0, lines : []}
-          wordMap[formattedWord]['count'] += 1
-          wordMap[formattedWord]['lines'].push(index + file.name.split('.')[0])
-        })
-        lineCount += 1
-      }
-    })
-
-    callback()
+function replaceWordInGroup (word) {
+  var replacedWord = word
+  _.each(GROUP_WORDS_DICTIONARY, function(value, key) {
+    if (_.some(value, function(mappedWord){ return mappedWord === word })) replacedWord = key
   })
-}, function() {
+  return replacedWord
+}
+
+function findPhrase (data, wordsToBeChecked) {
+
+  var phrases = []
+  data.forEach(function(line, index){
+    if(index === 0) return
+
+    var response = line.split(',')[file.index]
+    var wordArray = response.split(' ')
+    if(wordArray.length > 1 || wordArray[0] !== ''){
+
+      wordArray = wordArray.map(function(word){
+        var formattedWord = word.toLowerCase()
+        if(formattedWord.indexOf('.') !== -1) formattedWord = formattedWord.replace(/\./, '')
+        if(formattedWord.indexOf('"') !== -1 ) formattedWord = formattedWord.replace(/"/, '')
+        return formattedWord
+      })
+
+      if (_.every(wordsToBeChecked, function(word) {
+            if (GROUP_WORDS_DICTIONARY[word]){
+              return _.some(GROUP_WORDS_DICTIONARY[word], function (mappedWord) {
+                return wordArray.indexOf(mappedWord) !== -1
+              })
+            }
+            return wordArray.indexOf(word) !== -1
+          })
+         ) {
+        phrases.push(wordArray.join(' '))
+      }
+    }
+  })
+
+  return _.sample(phrases)
+}
+
+
+fs.readFile('./' + file.name, 'utf-8', function(err, data){
+  data = data.split('\n')
+  data.forEach(function(line, index){
+    if(index === 0) return
+
+    var response = line.split(',')[file.index]
+    var wordArray = response.split(' ')
+    if(wordArray.length > 1 || wordArray[0] !== ''){
+
+      wordArray = wordArray.map(function(word){
+        var formattedWord = word.toLowerCase()
+        if(formattedWord.indexOf('.') !== -1) formattedWord = formattedWord.replace(/\./, '')
+        if(formattedWord.indexOf('"') !== -1 ) formattedWord = formattedWord.replace(/"/, '')
+        formattedWord = replaceWordInGroup(formattedWord)
+
+        return formattedWord
+      })
+
+      var wordArrayFiltered = _.filter(wordArray, function(word){
+
+        if(exceptionWords.indexOf(word) !== -1 || casualException.indexOf(word) !== -1) return false
+
+        //formattedWord = groupWord(formattedWord)
+
+        if(!wordMap[word]) wordMap[word] = {count : 0, lines : []}
+        wordMap[word]['count'] += 1
+        wordMap[word]['lines'].push(index + file.name.split('.')[0])
+        return true
+      })
+
+      wordArrayFiltered.forEach(function(word, index){
+
+        if(exceptionWords.indexOf(word) !== -1 || casualException.indexOf(word) !== -1) return false
+
+        var wordArrayClone = _.clone(wordArrayFiltered)
+        wordArrayClone.splice(index, 1)
+        wordArrayClone.forEach(function(sinergyWord){
+          if(!sinergyWordMap[word]) sinergyWordMap[word] = {}
+          if(!sinergyWordMap[word][sinergyWord]) sinergyWordMap[word][sinergyWord] = 0
+          sinergyWordMap[word][sinergyWord] += 1
+        })
+      })
+
+      lineCount += 1
+    }
+    totalLineCount += 1
+  })
 
   _.each(wordMap, function(wordObject, word){
     var document = {
-      word: word,
-      count: wordObject.count,
-      lines: wordObject.lines
+      word : word, count : wordObject.count, lines : wordObject.lines
     }
     collection.push(document)
   })
 
-  var orderedCollection = _.sortBy(collection, function (document) {
-    return - document.count
+  _.each(sinergyWordMap, function(wordObject, word){
+    var sinergyCollection = []
+    _.each(wordObject, function(sinergyWordCount, word){
+      var document = { word : word, count : sinergyWordCount }
+      sinergyCollection.push(document)
+    })
+
+    sinergyWordMapFiltered[word] = _.sortBy(sinergyCollection, function(document){
+      return -document.count
+    })
+
   })
 
-  var orderedCollectionWithoutLines = orderedCollection.map(function (document) {
+  var orderedCollection = _.sortBy(collection, function(document){
+    return -document.count
+  })
+
+  var orderedCollectionWithoutLines = orderedCollection.map(function(document){
     return {
-      word : document.word,
-      count: document.count
+      word : document.word, count : document.count
     }
   })
-  console.log(orderedCollectionWithoutLines)
+
+  _.some(orderedCollectionWithoutLines, function(wordAndCount){
+    if (sinergyWordMapFiltered[wordAndCount.word]) {
+      sinergyWordMapFiltered[wordAndCount.word] = sinergyWordMapFiltered[wordAndCount.word].slice(0, MAX_WORDS_SINERGY)
+      sinergyWordMapFiltered[wordAndCount.word].forEach(function(wordAndCountSecond) {
+        var document = {}
+
+        document.word = GROUP_WORDS_DICTIONARY[wordAndCount.word] ?
+          GROUP_WORDS_DICTIONARY[wordAndCount.word].join(' / ') + ' (' + wordAndCount.count + ')' :
+          wordAndCount.word + ' (' + wordAndCount.count + ')'
+        document.secondword = wordAndCountSecond.word + ' (' + parseInt(wordAndCountSecond.count * 100 /wordAndCount.count) + '%)'
+        document.example = '"' + findPhrase(data, [wordAndCount.word, wordAndCountSecond.word]) + '"'
+        finalJsonToCsv.push(document)
+      })
+    } else {
+      finalJsonToCsv.push({ word: wordAndCount.word, secondword: undefined, example: undefined })
+    }
+    return wordAndCount.count <= parseInt(lineCount * MIN_WORD_COUNT)
+  })
+
+  var csv = json2csv({ data: finalJsonToCsv })
+  fs.writeFile('./test.csv', csv, function(err){
+    console.log('DONE WRITING')
+  })
+
   console.log('LINE COUNT: ', lineCount)
+  console.log('TOTAL LINE COUNT: ', totalLineCount)
 })
